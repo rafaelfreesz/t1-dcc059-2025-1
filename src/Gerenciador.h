32a59084902f34deb7628491a8b3e1445baa8a58
Samuel Paiva Bernardes <samuelpbernardes@yahoo.com.br>
2025-06-20 00:35:47 -0300

    Testes H

--- src/Gerenciador.h
+++ src/Gerenciador.h
@@ -13,6 +13,18 @@ class Gerenciador {
         static char get_id_entrada();
         static std::vector<char> get_conjunto_ids(Grafo *grafo, int tam);
         static bool pergunta_imprimir_arquivo(std::string nome_arquivo);
+
+    // Novo: Funções para cálculo das medidas
+    struct MedidasGrafo {
+        double raio;
+        double diametro;
+        std::vector<char> centro;
+        std::vector<char> periferia;
+    };
+    
+    static std::unordered_map<char, double> calcularDistanciasMinimas(Grafo* grafo, char origem);
+    static std::unordered_map<char, double> calcularExcentricidades(Grafo* grafo);
+    static MedidasGrafo calcularMedidas(Grafo* grafo);
     };
 
 
