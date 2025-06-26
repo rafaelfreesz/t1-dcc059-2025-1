22be04a734382b573dd736ad36f96dead21a8350
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-06-25 22:55:18 -0300

    trata exceções no comando de exec do programa

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <iterator>
 #include "Gerenciador.h"
 
 using namespace std;
 int main(int argc, char *argv[])
 {
+    if (argc == 0){
+        cout << "Erro: Nenhum grafo especificado" << endl;
+        exit(0);
+    }
+    if (argc > 2){
+        cout << "Erro: Mais de um grafo ao mesmo tempo" << endl;
+        exit(0);
+    }
+
+    
+    cout << "argc: " << argc << endl
+         << "argv: " << endl;
+    for(int i = 0; i < argc; i++)
+        cout << i << ": " << argv[i] <<endl;
+    
 
     Grafo* grafo = new Grafo();
 
