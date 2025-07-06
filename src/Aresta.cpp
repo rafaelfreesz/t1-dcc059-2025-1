0c8e5cf8fb9ac07bba9aab35e6c599ef52136cf4
BernardoRigolon <berigolon@gmail.com>
2025-07-06 01:17:05 -0300

    add aresta

--- src/Aresta.cpp
+++ src/Aresta.cpp
@@ -1,1 +1,43 @@
 #include "Aresta.h"
+
+Aresta::Aresta(char id_no_origem, char id_no_alvo, int peso)
+{
+    this->id_no_origem = id_no_origem;
+    this->id_no_alvo = id_no_alvo;
+    this->peso = peso;
+}
+
+Aresta::~Aresta()
+{
+
+}
+
+char Aresta::getIdOrigem()
+{
+    return this->id_no_origem;
+}
+
+char Aresta::getIdAlvo()
+{
+    return this->id_no_alvo;
+}
+
+int Aresta::getPeso()
+{
+    return this->peso;
+}
+
+void Aresta::setIdOrigem(char id_no_origem)
+{
+    this->id_no_origem = id_no_origem;
+}
+
+void Aresta::setIdAlvo(char id_no_alvo)
+{
+    this->id_no_alvo = id_no_alvo;
+}
+
+void Aresta::setPeso(int peso)
+{
+    this->peso = peso;
+}
--- src/Aresta.h
+++ src/Aresta.h
@@ -3,13 +3,21 @@
 
 using namespace std;
 class Aresta {
-public:
-    Aresta();
-    ~Aresta();
-
+private:
+    char id_no_origem;
     char id_no_alvo;
     int peso;
-    
+
+public:
+    Aresta(char id_no_origem, char id_no_alvo, int peso);
+    ~Aresta();
+
+    char getIdOrigem();
+    char getIdAlvo();
+    int getPeso();
+    void setIdOrigem(char id_no_origem);
+    void setIdAlvo(char id_no_alvo);
+    void setPeso(int peso);
 };
 
 
